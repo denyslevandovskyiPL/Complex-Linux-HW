ecfcee32d40286196bd51f372b4426da12447114
denys.levandovskyi <denys.levandovskyi@plvision.com>
2022-11-11 14:32:17 +0200

    COMPLEX LINUX HW
    -Clean old files from root project directory
    
    Autor: denys.levandovskyi@plvision.eu

--- codes.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef CODES_H
-#define CODES_H
-
-#define ERROR_CODE 0
-#define SUCCESS_CODE 1
-
-#endif //CODES_H
--- getUserInfo.h
+++
@@ -1,7 +0,0 @@
-#ifndef GETUSERINFO_H
-#define GETUSERINFO_H
-
-int readInteger(float *number);
-int getUserInfo(float* n1, float* n2, char** operation);
-
-#endif // GETUSERINFO_H
